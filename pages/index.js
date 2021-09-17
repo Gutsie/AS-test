8b7eed219d395348238f5035be0c14126f8677af
Richard Eriksson <richard@r-3.se>
2021-09-17 14:24:39 +0200

    TEST3#

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { useEffect,useState } from 'react'
 
-export default function Home() {
-  const [matches,setMatches] = useState([])
-
-  useEffect(()=>{
-    if(matches.length < 1){
-      console.log('Hämtar matcher')
-      fetch('/api/hello').then(res=> res.json()).then(json => setMatches(json.matches))
-    }
-  },[])
-  console.log(matches)
+export default function Home(props) {
   return (
     <div className="bg-blue-100 w-full min-h-screen flex flex-wrap p-2">
-      {matches.map((match)=>(
+      {props.matches.map((match)=>(
         <div key={match.teams} className="bg-white rounded-md p-4 m-2 shadow-md">
           <h2>{match.teams}</h2>
           <p>{match.date}</p>
@@ -22,4 +13,11 @@ export default function Home() {
       ))}
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const data = await fetch('http://localhost:3000/api/hello').then(res=> res.json()).then(json => json.matches)
+  return {
+    props: {matches:data}, // will be passed to the page component as props
+  }
 }
