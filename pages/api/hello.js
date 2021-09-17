3342de7f9207be25feb1790419a3fe3bac6445bc
Richard Eriksson <richard@r-3.se>
2021-09-17 13:51:19 +0200

    TEST

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,33 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const axios = require("axios");
+const cheerio = require("cheerio");
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const url = "https://www.allsvenskan.se/matcher";
+
+export default async function handler(req, res) {
+  const { data } = await axios.get(url);
+  // Load HTML we fetched in the previous line
+  const $ = cheerio.load(data);
+  // Select all the list items in plainlist class
+  const listItems = $(".matchesRounds a.item");
+  // Stores data for all countries
+  const countries = [];
+  // Use .each method to loop through the li we selected
+  listItems.each((idx, el) => {
+    // Object holding data for each country/jurisdiction
+    const country = { teams: "", date: "",round:"" };
+    // Select the text content of a and span elements
+    // Store the textcontent in the above object
+    country.teams = $(el).find("div.teams").text();
+    country.date = $(el).find("div.date").last().text();
+    country.round = $(el).prevAll('h3.title').first().text();
+    if(req.query['filter']){
+      if(country.teams.toLowerCase().indexOf(req.query['filter']) > -1){
+        countries.push(country);
+      }
+    }else{
+      countries.push(country);
+    }
+
+  });
+  res.status(200).json({ matches: countries})
 }
